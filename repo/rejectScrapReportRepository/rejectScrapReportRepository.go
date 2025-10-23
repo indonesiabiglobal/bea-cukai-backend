1e6f12d0d824cca0572c8c127e4aa69b8d938bd4
RafliFerdian25 <ferdianrafli25@gmail.com>
2025-10-23 22:39:54 +0700

    fix: update calculations in Excel export and adjust date formatting in SQL query

--- repo/rejectScrapReportRepository/rejectScrapReportRepository.go
+++ repo/rejectScrapReportRepository/rejectScrapReportRepository.go
@@ -39,7 +39,7 @@ func (r *RejectScrapReportRepository) getMaxScrapOpnameDate(ctx context.Context,
 	err := r.db.WithContext(ctx).
 		Table("tr_inv_opname_head").
 		Select("IFNULL(MAX(trans_date), '2000-01-01') as trans_date").
-		Where("trans_date <= ? AND item_group = 'SCRAP'", beforeDate).
+		Where("trans_date <= ? AND item_group = 'SCRAP'", beforeDate.Format("2006-01-02")).
 		Scan(&result).Error
 
 	if err != nil {
